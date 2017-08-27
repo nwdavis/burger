d7e302a8bb2cbe759cb417d48f10d3370883ca6b
nwdavis <nathanieldavis89@gmail.com>
2017-08-27 17:06:09 -0400

    adding html, building out some of the files

---
+++ config/orm.js
@@ -0,0 +1,20 @@
+var connection = require('./connection.js');
+
+var orm = {
+  selectAll: function(){
+    var queryString;
+    connection.query()
+
+  },
+  insertOne: function(){
+    var queryString;
+    connection.query()
+  },
+  updateOne: function(){
+    var queryString;
+    connection.query()
+  }
+
+};
+
+module.exports = orm;
