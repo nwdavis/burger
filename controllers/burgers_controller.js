51e99b3331618f6651bf99c430bed5c9e3ee0a6b
nwdavis <nathanieldavis89@gmail.com>
2017-08-30 21:20:22 -0400

    adding controller functions

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -4,8 +4,33 @@ var burger = require('../models/burger.js');
 var router = express.Router();
 
 router.get("/", function(req, res){
-    
-})
+  burger.all(function(data){
+    var hbsObj = {
+      burgers: data
+    };
+    res.render("index", hbsObj);
+  });
+});
+
+router.post("/", function(req, res){
+  burger.create([
+    "burger_name", "devoured"
+  ], [
+    req.body.burger_name, req.body.devoured
+  ], function() {
+    res.redirect("/");
+  });
+});
+
+router.put("/:id", function(req, res){
+  var condition = "id =" + req.params.id;
+
+  burger.update({
+    devoured: req.body.devoured
+  }, condition, function(){
+    res.redirect("/");
+  });
+});
 
 
 
