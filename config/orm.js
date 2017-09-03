d834e080f4034f2886f65c2333e9b1ca5f08e884
nwdavis <nathanieldavis89@gmail.com>
2017-09-03 19:03:25 -0400

    working on sql calls

--- config/orm.js
+++ config/orm.js
@@ -1,20 +1,70 @@
-var connection = require('./connection.js');
+var connection = require('../config/connection.js');
 
-var orm = {
-  selectAll: function(){
-    var queryString;
-    connection.query()
+function printQuestionMarks(num) {
+  var arr = [];
 
-  },
-  insertOne: function(){
-    var queryString;
-    connection.query()
-  },
-  updateOne: function(){
-    var queryString;
-    connection.query()
+  for (var i = 0; i < num; i++) {
+    arr.push("?");
   }
 
+  return arr.toString();
+}
+
+function objToSql(ob) {
+  var arr = [];
+
+  for (var key in ob) {
+    if (Object.hasOwnProperty.call(ob, key)) {
+      arr.push(key + "=" + ob[key]);
+    }
+  }
+
+  return arr.toString();
+}
+
+var orm = {
+  selectAll: function(tableInput, cb){
+    var queryString = "SELECT * FROM" + tableInput + ";";
+    connection.query(queryString, function(err, result){
+      if (err) {throw err;}
+      cb(result);
+    });
+  },
+  insertOne: function(table, cols, vals, cb){
+    var queryString = "INSERT INTO" + table;
+
+    queryString += " (";
+    queryString += cols.toString();
+    queryString += ") ";
+    queryString += "VALUES (";
+    queryString += printQuestionMarks(vals.length);
+    queryString += ") ";
+
+    console.log(queryString);
+
+    connection.query(queryString, vals, function(err, result) {
+      if (err) {
+        throw err;
+      }
+      cb(result);
+    });
+  },
+  updateOne: function(table, obvColVals, condition, cb){
+    var queryString = "UPDATE " + table;
+
+    queryString += " SET ";
+    queryString += objToSql(objColVals);
+    queryString += " WHERE ";
+    queryString += condition;
+
+    console.log(queryString);
+    connection.query(queryString, function(err, result) {
+      if (err) {
+        throw err;
+      }
+      cb(result);
+    });
+  }
 };
 
 module.exports = orm;
